482d8cad858d05147849b8653dfcbd5508908086
Camille Galladjov <ujinjinjin@outlook.com>
2025-05-07 01:30:30 +0200

    #7 WIP user creation

--- src/Auth.Wiedersehen/Controllers/Services/UserService.cs
+++ src/Auth.Wiedersehen/Controllers/Services/UserService.cs
@@ -1,11 +1,30 @@
 using Auth.Wiedersehen.Controllers.Models;
+using Auth.Wiedersehen.Database.Models;
+using Auth.Wiedersehen.Extensions;
+using Microsoft.AspNetCore.Identity;
 
 namespace Auth.Wiedersehen.Controllers.Services;
 
-internal sealed class UserService : IUserService
+internal sealed class UserService(UserManager<ApplicationUser> userManager) : IUserService
 {
-    public Task<CreateUserResult> CreateAsync(CreateUserRequest request)
+    private readonly UserManager<ApplicationUser> _userManager = userManager.Required(nameof(userManager));
+
+    public async Task<CreateUserResult> CreateAsync(CreateUserRequest request)
     {
-        throw new NotImplementedException();
+        var result = await _userManager.CreateAsync(
+            new ApplicationUser
+            {
+                Email = request.Email,
+                TermsAcceptanceTime = DateTime.UtcNow,
+            },
+            request.Password
+        );
+
+        if (!result.Succeeded)
+        {
+            throw new Exception("Failed to create user");
+        }
+
+        return new CreateUserResult();
     }
 }
